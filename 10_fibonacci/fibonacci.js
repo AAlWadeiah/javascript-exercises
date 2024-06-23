f800677ed643a90f8847b0cb44f23e8c222e5cfb
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-06-23 07:19:24 -0600

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  if (num == 0) return 0;
+  if (num == 1) return 1;
 
+  let result = 1;
+  let prev = 1;
+  for (let i = 2; i < +num; i++) {
+    let temp = result;
+    result += prev;
+    prev = temp;
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
