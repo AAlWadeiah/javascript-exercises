0a6fc83be50d2b5524e89be807a42c6e0bdcffdf
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-06-22 22:26:42 -0600

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toRemove) {
+  for (const item of toRemove) {
+    let i = 0;
+    while (i < arr.length) {
+      if (item === arr[i]) {
+        arr.splice(i, 1);
+      } else {
+        i++;
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
