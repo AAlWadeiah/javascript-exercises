bf40d3445bcc3e805f57429cefdc10be31d68208
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-06-23 08:08:24 -0600

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (personsArr) {
+  let ageArr = personsArr.map((person) => {
+    if ("yearOfDeath" in person) {
+      return person["yearOfDeath"] - person["yearOfBirth"];
+    } else {
+      let currYear = new Date().getFullYear();
+      return +currYear - person["yearOfBirth"];
+    }
+  });
+  let sortedArr = ageArr.toSorted((a, b) => a - b);
+  let maxAge = sortedArr.reverse()[0];
+  let index = ageArr.indexOf(maxAge);
+  return personsArr.at(index);
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
 // Do not edit below this line
 module.exports = findTheOldest;
