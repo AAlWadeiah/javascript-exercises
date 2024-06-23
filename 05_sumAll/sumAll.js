77531e55de38d89dcf2c0fdc7b984beb0e2da0fc
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-06-22 22:42:57 -0600

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  )
+    return "ERROR";
+  try {
+    let sum = 0;
+    let start = num1 < num2 ? num1 : num2;
+    let end = num1 > num2 ? num1 : num2;
+    for (i = start; i <= end; i++) sum += i;
+    return sum;
+  } catch (error) {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
